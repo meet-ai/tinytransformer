c3b603b6abb65c8eddb41c8733fc809a11aac210
meetai <meetai@gmx.com>
2024-03-05 17:37:09 +0800

    fix gen cn txt shell

--- scripts/spm_gen_cn_txt.py
+++ scripts/spm_gen_cn_txt.py
@@ -26,15 +26,15 @@ for d in data:
 with open("data/corpus_cn.txt", "w", encoding="utf-8") as fp:
     fp.write("\n".join(sentences))
     
-with open("data/en.txt", "r", encoding="utf-8") as fp:
-    data = fp.read().strip().split("\n")
-
-sentences = []
-for d in data:
-    d = d.strip()
-    if "===" in d or len(d) == 0 :
-        continue
-    sentences.append(d)
-
-with open("data/corpus_cn.txt", "w", encoding="utf-8") as fp:
-    fp.write("\n".join(sentences))
+#with open("data/cn.txt", "r", encoding="utf-8") as fp:
+#    data = fp.read().strip().split("\n")
+#
+#sentences = []
+#for d in data:
+#    d = d.strip()
+#    if "===" in d or len(d) == 0 :
+#        continue
+#    sentences.append(d)
+#
+#with open("data/corpus_cn.txt", "w", encoding="utf-8") as fp:
+#    fp.write("\n".join(sentences))
